c4d1f3b7c6100e0d1ed4af38c4426a20b5dae91e
NfSilveira <97970315+NfSilveira@users.noreply.github.com>
2022-04-01 18:52:57 -0300

    Create Atividades 1 e 2.c

---
+++ 210322/Atividades 1 e 2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void At1() {
+  printf("Nome: Nicolas Freitas Silveira\nTelefone: (11)94141-6237\nE-mail: nicolasfreitas.silveira@hotmail.com\nData de Nascimento: 20/06/2003\n");
+}
+void At2() {
+  printf("Digite 1 para Login..\n");
+  printf("\tDigite 2 para Pesquisar!\n");
+  printf("\t\tDigite 3 para Extensões!\n");
+  printf("\t\t\tDigite 4 para Ajustes!\n");
+}
+
+void main() {
+  At1():
+  At2();
+}
