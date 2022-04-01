fd8d82df4bba6366849e5d3cc4a1af9b816086b7
NfSilveira <97970315+NfSilveira@users.noreply.github.com>
2022-04-01 18:53:26 -0300

    Create Aula.c

---
+++ 210322/Aula.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/*
+Exemplos da aula do dia:
+21/03/2022
+  */
+
+
+int main(void) {
+  printf("Nicolas Freitas Silveira\n");
+  printf("Idade: 18 Anos\n");
+  printf("CEP: 00101-100\n");
+  printf("Endereço: Rua Karl Marx, 666\n");
+  //printf("");
+  return 0;
+}
