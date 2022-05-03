4b598e5403b14c94e53087326fce15cc97ff9e79
Nicolas Freitas Silveira <97970315+NfSilveira@users.noreply.github.com>
2022-05-03 11:22:41 -0300

    Delete list_comprehension1.py

--- list_comprehension1.py
+++
@@ -1,9 +0,0 @@
-fruits = ["apple", "pear", "banana"]
-
-for index, fruit in enumerate(fruits):
-    
-    if fruit == "apple":
-
-        fruits[index] = fruit + " - " + "red-fruit"
-
-print(fruits)
