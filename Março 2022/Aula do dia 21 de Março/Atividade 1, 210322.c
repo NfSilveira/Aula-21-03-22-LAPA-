c9201ec86f17d4b69c269b4acdc4ad8a0fbcb292
Nicolas Freitas Silveira <97970315+NfSilveira@users.noreply.github.com>
2022-06-17 09:54:51 -0300

    Create Atividade 1, 210322.c

---
+++ Março 2022/Aula do dia 21 de Março/Atividade 1, 210322.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void) {
+  printf("Nome: Nicolas Freitas Silveira\nTelefone: (11)94141-6237\nE-mail: nicolasfreitas.silveira@hotmail.com\nData de Nascimento: 20/06/2003\n");
+  return 0;
+}
