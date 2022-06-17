a55db22d2f836d4fe24832d2979f538489864444
Nicolas Freitas Silveira <97970315+NfSilveira@users.noreply.github.com>
2022-06-17 09:55:16 -0300

    Create Atividade 2, 210322.c

---
+++ Março 2022/Aula do dia 21 de Março/Atividade 2, 210322.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+  printf("Digite 1 para Login..\n");
+  printf("\tDigite 2 para Pesquisar!\n");
+  printf("\t\tDigite 3 para Extensões!\n");
+  printf("\t\t\tDigite 4 para Ajustes!\n");
+  return 0;
+}
